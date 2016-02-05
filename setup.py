185de029deef8270ec8b04354445d9ec5dc5814b
rerb <bob.erb@aashe.org>
2016-02-05 17:05:47 -0500

    Add support for aashe-tabs

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 
 
@@ -8,7 +8,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='aashe_theme',
-      version='1.9.3',
+      version='1.10.3',
       description='Unify, themed for AASHE',
       author='AASHE',
       author_email='webdev@aashe.org',
